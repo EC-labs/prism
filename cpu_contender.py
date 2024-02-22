c7faa999ffff8a665f8d458136ed6777fc724f69
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2024-02-22 19:17:04 +0100

    cpu-contention: add both cpu saturation scenarios

--- cpu_contender.py
+++ cpu_contender.py
@@ -2,8 +2,8 @@ import time
 
 while True: 
     start = time.time()
-    for i in range(10000000): 
+    for i in range(20000000): 
         pass
     elapsed_time = time.time() - start
     print(elapsed_time)
-    time.sleep(1-elapsed_time)
+    time.sleep(max(1-elapsed_time, 0))
--- workload-generator/src/main.rs
+++ workload-generator/src/main.rs
@@ -1,7 +1,7 @@
 use chrono::prelude::*;
 use eyre::Result;
 use reqwest::Client;
-use std::{fs::File, time::Duration};
+use std::{fs::File, iter, time::Duration};
 use tokio::{
     sync::mpsc::{self, Receiver, Sender},
     time::{self, Duration as tDuration},
@@ -11,7 +11,13 @@ use tokio::{
 async fn main() -> Result<()> {
     let (tx, mut rx): (Sender<(String, Duration)>, Receiver<(String, Duration)>) =
         mpsc::channel(1000);
-    for nreq in [1, 1, 2, 10, 10, 10] {
+
+    println!("Starting fase 1");
+    let mut req: Vec<u64> = iter::repeat(2).take(20).collect();
+    req.extend(iter::repeat(10).take(10));
+    req.extend(iter::repeat(1).take(30));
+    for nreq in req {
+        println!("Sending {:?}", nreq);
         for req_id in 0..nreq {
             let tx = tx.clone();
             let handle = tokio::spawn(async move {
@@ -29,13 +35,32 @@ async fn main() -> Result<()> {
                 tx.send((current_time.to_rfc3339(), response_time.to_std().unwrap()))
                     .await
                     .unwrap();
+            });
+        }
+        time::sleep(tDuration::from_millis(1000)).await;
+    }
 
-                println!(
-                    "req_id {:?}: {:?}-{:?}",
-                    req_id,
-                    response.status(),
-                    response_time.to_std().unwrap().as_millis()
-                );
+    let req: Vec<u64> = iter::repeat(4).take(60).collect();
+    println!("Starting fase 2");
+    for nreq in req {
+        println!("Sending {:?}", nreq);
+        for req_id in 0..nreq {
+            let tx = tx.clone();
+            let handle = tokio::spawn(async move {
+                let sleep_time = (1000 / nreq) * req_id;
+                time::sleep(tDuration::from_millis(sleep_time)).await;
+                let client = Client::new();
+                let start = Utc::now();
+                let response = client
+                    .get("http://localhost:7878/cpu")
+                    .send()
+                    .await
+                    .unwrap();
+                let current_time = Utc::now();
+                let response_time = current_time.signed_duration_since(start);
+                tx.send((current_time.to_rfc3339(), response_time.to_std().unwrap()))
+                    .await
+                    .unwrap();
             });
         }
         time::sleep(tDuration::from_millis(1000)).await;
