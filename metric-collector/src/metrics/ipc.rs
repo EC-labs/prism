17eab1c2ac7d03e94d58a5dac2acccfe61277021
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2024-04-24 11:33:30 +0200

    collect: Store stream metrics

--- metric-collector/src/metrics/ipc.rs
+++ metric-collector/src/metrics/ipc.rs
@@ -4,6 +4,7 @@ use std::{
     cell::RefCell,
     collections::{HashMap, HashSet},
     fs::{self, File},
+    hash::RandomState,
     io::prelude::*,
     mem,
     path::Path,
@@ -23,10 +24,10 @@ type KFile = (u32, u64);
 
 pub struct Ipc {
     ipc_program: Rc<RefCell<IpcProgram>>,
-    file_map: HashMap<KFile, u64>,
-    sum_file_wait_time_ns: u64,
+    stream_map: HashMap<KFile, u64>,
+    sum_stream_wait_ns: u64,
     pending: HashMap<KFile, u64>,
-    last_updated: HashSet<KFile>,
+    last_terminated: HashSet<KFile>,
     tid: usize,
     data_files: LruCache<String, File>,
     sample_instant_ns: Option<u128>,
@@ -43,10 +44,10 @@ impl Ipc {
         Self {
             ipc_program,
             tid,
-            file_map: HashMap::new(),
-            sum_file_wait_time_ns: 0,
+            stream_map: HashMap::new(),
+            sum_stream_wait_ns: 0,
             pending: HashMap::new(),
-            last_updated: HashSet::new(),
+            last_terminated: HashSet::new(),
             data_files: LruCache::with_expiry_duration(Duration::from_millis(120)),
             sample_instant_ns: None,
             target_subdirectory: format!("{}/{}/ipc", root_directory, target_subdirectory),
@@ -54,7 +55,6 @@ impl Ipc {
     }
 
     fn process_event(&mut self, event: IpcEvent) {
-        println!("{:?}", event);
         match event {
             IpcEvent::ReadStart {
                 sb_id,
@@ -69,7 +69,6 @@ impl Ipc {
                 ..
             } => {
                 self.pending.insert((sb_id, inode_id), ns_since_boot);
-                self.last_updated.insert((sb_id, inode_id));
             }
             IpcEvent::ReadEnd {
                 sb_id,
@@ -83,10 +82,12 @@ impl Ipc {
                 ns_elapsed,
                 ..
             } => {
-                let entry = self.file_map.entry((sb_id, inode_id)).or_insert(0);
-                self.last_updated.insert((sb_id, inode_id));
+                let kfile = (sb_id, inode_id);
+                let entry = self.stream_map.entry(kfile).or_insert(0);
+                self.last_terminated.insert(kfile);
+                self.pending.remove(&kfile);
                 *entry += ns_elapsed;
-                self.sum_file_wait_time_ns += ns_elapsed;
+                self.sum_stream_wait_ns += ns_elapsed;
             }
             _ => {
                 println!("{:?}", event);
@@ -114,6 +115,61 @@ impl Ipc {
         let file = self.data_files.get(filepath.to_str().unwrap());
         Ok(file.unwrap())
     }
+
+    fn store_streams(&mut self, epoch_ns: u128) -> Result<()> {
+        let last_terminated = mem::replace(&mut self.last_terminated, HashSet::new());
+        let keys = self.pending.keys().into_iter().map(|key| (key.0, key.1));
+        let kfiles: HashSet<KFile> = HashSet::from_iter(keys);
+        let kfiles = kfiles.union(&last_terminated);
+
+        for kfile in kfiles {
+            let epoch_ms = epoch_ns / 1_000_000;
+            let pending = if let Some(ns_since_boot) = self.pending.get(&kfile) {
+                epoch_ns - (*BOOT_EPOCH_NS.read().unwrap() + *ns_since_boot as u128)
+            } else {
+                0
+            };
+            let cached_wait = *self.stream_map.get(&kfile).unwrap_or(&0);
+            let cumulative_wait = pending as u64 + cached_wait;
+
+            let sample = StreamFileSample {
+                epoch_ms,
+                cumulative_wait,
+            };
+
+            let file_path = format!(
+                "{}/streams/{:?}/{:?}_{:?}.csv",
+                self.target_subdirectory,
+                (epoch_ms / (1000 * 60)) * 60,
+                kfile.0,
+                kfile.1,
+            );
+            let mut file = self.get_or_create_file(Path::new(&file_path), sample.csv_headers())?;
+            file.write_all(sample.to_csv_row().as_bytes())?;
+        }
+        Ok(())
+    }
+
+    fn store_aggregated_stream(&mut self, epoch_ns: u128) -> Result<()> {
+        let epoch_ms = epoch_ns / 1_000_000;
+        let mut cumulative_wait = self.sum_stream_wait_ns;
+        for (_, ns_since_boot) in self.pending.iter() {
+            cumulative_wait +=
+                (epoch_ns - (*BOOT_EPOCH_NS.read().unwrap() + *ns_since_boot as u128)) as u64;
+        }
+        let sample = StreamAggregatedSample {
+            epoch_ms,
+            cumulative_wait,
+        };
+        let file_path = format!(
+            "{}/streams/{:?}/total.csv",
+            self.target_subdirectory,
+            (epoch_ms / (1000 * 60)) * 60,
+        );
+        let mut file = self.get_or_create_file(Path::new(&file_path), sample.csv_headers())?;
+        file.write_all(sample.to_csv_row().as_bytes())?;
+        Ok(())
+    }
 }
 
 impl Collect for Ipc {
@@ -136,7 +192,7 @@ impl Collect for Ipc {
     }
 
     fn store(&mut self) -> Result<()> {
-        if self.last_updated.len() == 0 {
+        if self.last_terminated.len() == 0 && self.pending.len() == 0 {
             return Ok(());
         }
 
@@ -144,43 +200,35 @@ impl Collect for Ipc {
             .sample_instant_ns
             .take()
             .ok_or_eyre("Missing sample instant")?;
-        let epoch_ms = epoch_ns / 1_000_000;
 
-        let last_updated = mem::replace(&mut self.last_updated, HashSet::new());
-        for kfile in last_updated {
-            let pending = if let Some(ns_since_boot) = self.pending.get(&kfile) {
-                epoch_ns - (*BOOT_EPOCH_NS.read().unwrap() + *ns_since_boot as u128)
-            } else {
-                0
-            };
-            let cached_wait = *self.file_map.get(&kfile).unwrap_or(&0);
-            let cumulative_wait = pending as u64 + cached_wait;
+        self.store_streams(epoch_ns)?;
+        self.store_aggregated_stream(epoch_ns)?;
 
-            let sample = IpcFileSample {
-                epoch_ms,
-                cumulative_wait,
-            };
-
-            let file_path = format!(
-                "{}/streams/{:?}/{:?}_{:?}.csv",
-                self.target_subdirectory,
-                (epoch_ms / (1000 * 60)) * 60,
-                kfile.0,
-                kfile.1,
-            );
-            let mut file = self.get_or_create_file(Path::new(&file_path), sample.csv_headers())?;
-            file.write_all(sample.to_csv_row().as_bytes())?;
-        }
         Ok(())
     }
 }
 
-struct IpcFileSample {
+struct StreamAggregatedSample {
     epoch_ms: u128,
     cumulative_wait: u64,
 }
 
-impl ToCsv for IpcFileSample {
+impl ToCsv for StreamAggregatedSample {
+    fn csv_headers(&self) -> &'static str {
+        "epoch_ms,stream_wait\n"
+    }
+
+    fn to_csv_row(&self) -> String {
+        format!("{},{}\n", self.epoch_ms, self.cumulative_wait)
+    }
+}
+
+struct StreamFileSample {
+    epoch_ms: u128,
+    cumulative_wait: u64,
+}
+
+impl ToCsv for StreamFileSample {
     fn to_csv_row(&self) -> String {
         format!("{},{}\n", self.epoch_ms, self.cumulative_wait)
     }
