84f8925847c97eab8434a0d6a8eee42a20551941
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2024-07-03 22:27:30 +0200

    Adapt futex metrics to new in-kernel bpf stats

--- metric-collector/src/metrics/futex.rs
+++ metric-collector/src/metrics/futex.rs
@@ -2,6 +2,7 @@ use eyre::Result;
 use lru::LruCache;
 use std::{
     cell::RefCell,
+    collections::{HashMap, VecDeque},
     error::Error,
     fmt,
     fs::{self, File},
@@ -14,10 +15,7 @@ use std::{
 use super::{Collect, ToCsv};
 use crate::execute::{
     boot_to_epoch,
-    programs::{
-        futex::{FutexEvent, FutexProgram},
-        BOOT_EPOCH_NS,
-    },
+    programs::futex::{FutexEvent, FutexProgram},
 };
 
 #[derive(Debug)]
@@ -31,17 +29,30 @@ impl fmt::Display for UnwritableEvent {
 
 impl Error for UnwritableEvent {}
 
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct FutexKey {
+    root_pid: usize,
+    uaddr: Rc<str>,
+}
+
+#[derive(Clone)]
+struct WaitStat {
+    accumulated_wait: u64,
+    count: usize,
+}
+
+enum SnapshotStat {
+    Wait(WaitStat),
+    Wake { count: usize },
+}
+
 pub struct Futex {
-    futex_program: Rc<RefCell<FutexProgram>>,
     tid: usize,
-    sum_futex_wait_time: u128,
-    futex_wait_start: Option<u128>,
-    futex_sample: Option<FutexSample>,
-    events: Option<Vec<FutexEvent>>,
-    current_start: Option<FutexEvent>,
+    futex_program: Rc<RefCell<FutexProgram>>,
+    futex_stats_map: HashMap<FutexKey, WaitStat>,
+    snapshots: HashMap<FutexKey, VecDeque<(u64, SnapshotStat)>>,
     data_files: LruCache<String, File>,
     target_subdirectory: String,
-    root_directory: Rc<str>,
 }
 
 impl Futex {
@@ -52,21 +63,21 @@ impl Futex {
         target_subdirectory: &str,
     ) -> Self {
         Self {
-            futex_program,
             tid,
-            sum_futex_wait_time: 0,
-            futex_wait_start: None,
-            futex_sample: None,
-            events: None,
-            current_start: None,
+            futex_program,
+            futex_stats_map: HashMap::new(),
+            snapshots: HashMap::new(),
             data_files: LruCache::new(NonZeroUsize::new(4).unwrap()),
             target_subdirectory: format!("{}/{}/futex", root_directory, target_subdirectory),
-            root_directory: root_directory.clone(),
         }
     }
 
-    fn get_or_create_file(&mut self, filepath: &Path, headers: &str) -> Result<&File> {
-        let file = self.data_files.get(filepath.to_str().unwrap());
+    fn get_or_create_file<'a>(
+        data_files: &'a mut LruCache<String, File>,
+        filepath: &Path,
+        headers: &str,
+    ) -> Result<&'a File> {
+        let file = data_files.get(filepath.to_str().unwrap());
         if let None = file {
             let file = File::options().append(true).open(filepath);
             let file = match file {
@@ -78,143 +89,160 @@ impl Futex {
                 }
                 Ok(file) => file,
             };
-            self.data_files.put(filepath.to_str().unwrap().into(), file);
+            data_files.put(filepath.to_str().unwrap().into(), file);
         }
 
-        let file = self.data_files.get(filepath.to_str().unwrap());
+        let file = data_files.get(filepath.to_str().unwrap());
         Ok(file.unwrap())
-    }
-
-    fn store_sample(&mut self, sample: FutexSample) -> Result<()> {
-        let epoch_ms = sample.epoch_ms;
-        let row = sample.to_csv_row();
-        let day_epoch = (epoch_ms / (1000 * 60 * 60 * 24)) * (1000 * 60 * 60 * 24);
-        let filepath = format!("{}/{}.csv", self.target_subdirectory, day_epoch);
-        let filepath = Path::new(&filepath);
-        let mut file = self.get_or_create_file(filepath, &sample.csv_headers())?;
-        file.write_all(row.as_bytes())?;
-
-        Ok(())
-    }
-
-    fn write_event(&mut self, event: &FutexEvent) -> Result<()> {
-        let filename = match event {
-            FutexEvent::Elapsed { ns_since_boot, .. } => {
-                let epoch_ns = *BOOT_EPOCH_NS.read().unwrap() + ns_since_boot;
-                let epoch_minute_s = (epoch_ns / (1_000_000_000 * 60)) * 60;
-                format!(
-                    "{}/global/futex/wait/{}/{}.csv",
-                    self.root_directory, self.tid, epoch_minute_s,
-                )
-            }
-            FutexEvent::Wake {
-                root_pid,
-                uaddr,
-                ns_since_boot,
-                ..
-            } => {
-                let epoch_ns = *BOOT_EPOCH_NS.read().unwrap() + ns_since_boot;
-                let epoch_minute_s = (epoch_ns / (1_000_000_000 * 60)) * 60;
-                format!(
-                    "{}/global/futex/wake/{}/{}/{}.csv",
-                    self.root_directory, root_pid, uaddr, epoch_minute_s,
-                )
-            }
-            _ => return Err(UnwritableEvent.into()),
-        };
-
-        let mut file = self.get_or_create_file(&Path::new(&filename), event.csv_headers())?;
-        file.write_all(event.to_csv_row().as_bytes())?;
-        Ok(())
-    }
-
-    fn store_events(&mut self) -> Result<()> {
-        for event in self.events.take().unwrap() {
-            match event {
-                FutexEvent::Start { .. } => {
-                    self.current_start = Some(event);
-                }
-                FutexEvent::Elapsed { ret, .. } => {
-                    self.current_start.take().unwrap();
-                    if ret == 0 {
-                        self.write_event(&event)?;
-                    }
-                }
-                FutexEvent::Wake { ret, .. } => {
-                    if ret > 0 {
-                        self.write_event(&event)?;
-                    }
-                }
-                _ => {}
-            }
-        }
-        Ok(())
     }
 }
 
 impl Collect for Futex {
     fn sample(&mut self) -> Result<()> {
-        let res = self
+        let events = self
             .futex_program
             .borrow_mut()
             .take_futex_events(self.tid)?;
-        let time_since_boot = res.1;
-        self.events = Some(res.0);
 
-        for event in self.events.as_ref().unwrap() {
+        for event in events {
             match event {
-                FutexEvent::Start { ns_since_boot, .. } => {
-                    self.futex_wait_start = Some(*ns_since_boot);
+                FutexEvent::Wait {
+                    root_pid,
+                    uaddr,
+                    sample_instant_ns,
+                    total_interval_wait_ns,
+                    count,
+                    ..
+                } => {
+                    let futex = FutexKey { root_pid, uaddr };
+                    let stat = self
+                        .futex_stats_map
+                        .entry(futex.clone())
+                        .or_insert(WaitStat {
+                            accumulated_wait: 0,
+                            count: 0,
+                        });
+                    *stat = WaitStat {
+                        accumulated_wait: stat.accumulated_wait + total_interval_wait_ns,
+                        count,
+                    };
+                    let snapshot = self
+                        .snapshots
+                        .entry(futex)
+                        .or_insert_with(|| VecDeque::new());
+                    snapshot.push_back((sample_instant_ns, SnapshotStat::Wait(stat.clone())));
                 }
-                FutexEvent::Elapsed { ns_elapsed, .. } => {
-                    self.futex_wait_start = None;
-                    self.sum_futex_wait_time += ns_elapsed;
+                FutexEvent::Wake {
+                    root_pid,
+                    uaddr,
+                    sample_instant_ns,
+                    count,
+                    ..
+                } => {
+                    let futex = FutexKey { root_pid, uaddr };
+                    let snapshot = self
+                        .snapshots
+                        .entry(futex)
+                        .or_insert_with(|| VecDeque::new());
+                    snapshot.push_back((sample_instant_ns, SnapshotStat::Wake { count }))
                 }
-                _ => {}
             }
         }
-
-        let time_since_wait_start = if let Some(wait_start) = self.futex_wait_start {
-            time_since_boot.expect("Futex latest instant") - wait_start
-        } else {
-            0
-        };
-
-        self.futex_sample = if let Some(ns_since_boot) = time_since_boot {
-            Some(FutexSample {
-                epoch_ms: boot_to_epoch(ns_since_boot) / 1_000_000,
-                cumulative_futex_wait: self.sum_futex_wait_time + time_since_wait_start,
-            })
-        } else {
-            None
-        };
 
         Ok(())
     }
 
     fn store(&mut self) -> Result<()> {
-        if let None = self.futex_sample {
+        if self.snapshots.len() == 0 {
             return Ok(());
         }
-        let sample = self.futex_sample.take().unwrap();
 
-        self.store_sample(sample)?;
-        self.store_events()
+        for (futex, snapshots) in self.snapshots.iter_mut() {
+            while let Some((sample_instant_ns, snapshot)) = snapshots.pop_front() {
+                let sample_epoch_ms = boot_to_epoch(sample_instant_ns as u128) / 1_000_000;
+                let (sample, filename): (Box<dyn ToCsv>, String) = match snapshot {
+                    SnapshotStat::Wait(wait) => {
+                        let sample = Box::new(FutexWaitSample {
+                            epoch_ms: sample_epoch_ms,
+                            cumulative_futex_wait: wait.accumulated_wait,
+                            count: wait.count,
+                        });
+                        let filename = format!(
+                            "{}/wait/{}/{}-{}",
+                            self.target_subdirectory,
+                            (sample_epoch_ms / (1000 * 60)) * 60,
+                            futex.root_pid,
+                            futex.uaddr,
+                        );
+                        (sample, filename)
+                    }
+                    SnapshotStat::Wake { count } => {
+                        let sample = Box::new(FutexWakeSample {
+                            epoch_ms: boot_to_epoch(sample_instant_ns as u128),
+                            count,
+                        });
+                        let filename = format!(
+                            "{}/wake/{}/{}-{}",
+                            self.target_subdirectory,
+                            (sample_epoch_ms / (1000 * 60)) * 60,
+                            futex.root_pid,
+                            futex.uaddr,
+                        );
+                        (sample, filename)
+                    }
+                };
+                let mut file = Self::get_or_create_file(
+                    &mut self.data_files,
+                    Path::new(&filename),
+                    sample.csv_headers(),
+                )?;
+                file.write_all(sample.to_csv_row().as_bytes())?;
+            }
+        }
+
+        Ok(())
     }
 }
 
 #[derive(Debug)]
-struct FutexSample {
+struct FutexWaitSample {
     epoch_ms: u128,
-    cumulative_futex_wait: u128,
+    cumulative_futex_wait: u64,
+    count: usize,
 }
 
-impl ToCsv for FutexSample {
+impl ToCsv for FutexWaitSample {
     fn csv_headers(&self) -> &'static str {
-        "epoch_ms,futex_wait\n"
+        "epoch_ms,futex_wait_ns,futex_count\n"
     }
 
     fn to_csv_row(&self) -> String {
-        format!("{},{}\n", self.epoch_ms, self.cumulative_futex_wait)
+        format!(
+            "{},{},{}\n",
+            self.epoch_ms, self.cumulative_futex_wait, self.count
+        )
     }
+}
+
+#[derive(Debug)]
+struct FutexWakeSample {
+    epoch_ms: u128,
+    count: usize,
+}
+
+impl ToCsv for FutexWakeSample {
+    fn csv_headers(&self) -> &'static str {
+        "epoch_ms,futex_count\n"
+    }
+
+    fn to_csv_row(&self) -> String {
+        format!("{},{}\n", self.epoch_ms, self.count)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn tmp() {}
 }
