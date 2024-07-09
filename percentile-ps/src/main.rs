04223fe9cff5c69a0d77dcb7b9812f16420d57eb
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2024-07-09 13:03:43 +0100

    Add operation throughput computation from raw request

--- percentile-ps/src/main.rs
+++ percentile-ps/src/main.rs
@@ -1,66 +1,26 @@
-use hdrhistogram::Histogram;
-use std::{
-    collections::{BTreeMap, HashSet},
-    io,
-    ops::Bound::Included,
-};
+use eyre::Result;
 
-fn main() {
-    let mut start_map = BTreeMap::new();
-    let mut end_map = BTreeMap::new();
-    let time_factor = 1_000_000u64;
-    let mut buf = String::new();
-    let lines = 0;
+mod cli;
+mod config;
+mod program;
 
-    println!("Reading input");
-    for (i, line) in io::stdin().lines().enumerate() {
-        let line = match line {
-            Ok(c) => {
-                if c.len() == 0 {
-                    break;
-                } else {
-                    c
-                }
-            }
-            Err(_) => break,
-        };
+use config::Config;
+use program::{response_time::PercentileResponseTime, throughput_ops::ThroughputOps};
 
-        if i % 500_000 == 0 {
-            println!("Read {:?} lines", i)
+fn main() -> Result<()> {
+    let cmd = cli::register_args();
+    let config = Config::try_from(cmd.get_matches())?;
+    match config {
+        Config::ResponseTimePercentile {
+            percentile,
+            time_factor,
+        } => {
+            PercentileResponseTime::run(time_factor, percentile);
         }
-
-        let line = line.trim();
-        let mut elements = line.split(",");
-
-        let start: u64 = elements.next().unwrap().parse().unwrap();
-        let end: u64 = elements.next().unwrap().parse().unwrap();
-        let delay: u64 = elements.next().unwrap().parse().unwrap();
-
-        start_map.insert(start, (start, end, delay));
-        end_map.insert(end, (start, end, delay));
-
-        buf.truncate(0);
+        Config::ThroughputOps { time_factor } => {
+            ThroughputOps::run(time_factor);
+        }
     }
 
-    let (start_epoch, end_epoch) = (
-        start_map.first_key_value().unwrap().0,
-        end_map.last_key_value().unwrap().0,
-    );
-
-    for current_epoch in (*start_epoch..=*end_epoch).step_by(time_factor as usize) {
-        let combined_set = start_map
-            .range(..=current_epoch)
-            .map(|(_, v)| v)
-            .filter(|v| v.1 > current_epoch - time_factor);
-
-        let mut hist = Histogram::<u64>::new_with_bounds(1, 1000 * 1000 * 60, 2).unwrap();
-        for (_, _, rt) in combined_set {
-            hist.record(*rt).unwrap();
-        }
-
-        let qv = hist.value_at_quantile(0.95);
-        println!("{:?},{:?}", current_epoch / time_factor, qv);
-    }
-    // let mut hist = Histogram::<u64>::new_with_bounds(1, 1000 * 1000 * 60, 2).unwrap();
-    // hist.record(54321).unwrap();
+    Ok(())
 }
