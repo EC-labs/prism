833a80bcfd62e8c5644f1cf9fbf29b18b716127b
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2025-01-14 11:04:49 +0100

    Add redis benchmark

--- benchmarks/dependencies/cpu_contender/run.sh
+++ benchmarks/dependencies/cpu_contender/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash 
 
+set -e
+
+script_d="$(realpath "$(dirname $0)")"
 child_pids=()
 
 function kill_child_pids() {
     kill -9 "${child_pids[@]}" 
 }
 
-trap kill_child_pids SIGINT
+trap kill_child_pids SIGTERM
 
-python cpu_contender.py &
+python "${script_d}/cpu_contender.py" &
 child_pids+=($!)
 echo ${child_pids[@]}
 taskset -p 0x1 "${child_pids[-1]}"
