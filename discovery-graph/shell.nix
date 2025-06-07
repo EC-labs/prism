77d10e3d4e493282d51ee2fc1807cffa34826909
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-06-07 15:31:57 +0200

    Add discovery graph creation scripts

---
+++ discovery-graph/shell.nix
@@ -0,0 +1,17 @@
+let
+    # We pin to a specific nixpkgs commit for reproducibility.
+    # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+    pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+    packages = with pkgs; [
+        (python3.withPackages (python-pkgs: with python-pkgs; [
+            # select Python packages here
+            graphviz
+            pyvis
+            duckdb
+            plotly 
+            networkx 
+            pandas
+        ]))
+    ];
+}
