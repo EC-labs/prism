408d97cbf8d26e93d5ebaacc37f7c27d84ca6fca
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-06-26 18:21:14 +0200

    Add direnv

--- flake.nix
+++ flake.nix
@@ -21,6 +21,36 @@
                         Entrypoint = [ "/bin/metric-collector" ];
                     };
                 };
+                shell = with pkgs; stdenv.mkDerivation {
+                    name = "prism";
+                    hardeningDisable = [ "stackprotector" "zerocallusedregs" ];
+
+                    nativeBuildInputs = [ 
+                        rustfmt
+                        rust-analyzer
+                        rustc
+                        cargo
+                        elfutils
+                        libz
+                        pkg-config 
+                        libclang.lib
+                        clang
+                        bpftrace
+                        linuxHeaders
+                        zsh
+                        strace
+                        netcat-openbsd
+                        tcpdump
+                    ];
+
+                    buildInputs = [ 
+                        duckdb
+                        openssl
+                        protobuf
+                    ];
+
+                    LIBCLANG_PATH = "${libclang.lib}/lib";
+                };
             }
         );
 }
