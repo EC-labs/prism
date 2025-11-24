c56b557eb598ac1b3e5a4495679fd9f852e9a332
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-11-24 10:10:35 +0100

    Performance analysis dashboard setup

---
+++ analysis/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, pyproject-build-systems, pyproject-nix, lib, uv2nix }:
+let 
+      python = pkgs.python312;
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+      pythonSet = (pkgs.callPackage pyproject-nix.build.packages { inherit python; }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.wheel
+              overlay
+            ]
+          );
+      virtualenv = pythonSet.mkVirtualEnv "dev-env" workspace.deps.all;
+in
+pkgs.mkShell {
+    packages = [
+      python
+      pkgs.uv
+      virtualenv
+    ];
+    env = {
+      UV_NO_SYNC = "1";
+      UV_PYTHON = pythonSet.python.interpreter;
+      UV_PYTHON_DOWNLOADS = "never";
+      LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+    };
+    shellHook = ''
+      unset PYTHONPATH
+
+      cd "$(git rev-parse --show-toplevel)/analysis"
+      uv sync
+      source .venv/bin/activate
+      cd -
+    '';
+}
---
+++ analysis/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Uber pickups in NYC')
+
+def main():
+    print("Hello from analysis!")
+
+
+if __name__ == "__main__":
+    main()
