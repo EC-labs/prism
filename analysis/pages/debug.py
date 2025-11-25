9a316df72bf0792f27d2776116f7abac73114605
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-11-25 09:45:18 +0100

    analysis: Add linechart to debug

--- analysis/pages/debug.py
+++ analysis/pages/debug.py
@@ -1,23 +1,20 @@
 import streamlit as st
-import pandas as pd
-from streamlit_monaco import st_monaco
-import pandas as pd
-import streamlit as st
-from numpy.random import default_rng as rng
+import matplotlib
+import matplotlib.pyplot as plt
 
 from components.monaco_sql_editor import monaco_sql_editor
 from database import DatabaseClient
 
-db = DatabaseClient("../data/prism-2025-11-21T11:46:41.944295953+00:00.db3")
+matplotlib.style.use("dark_background")
+
+db = DatabaseClient("../data/prism-2025-11-24T13:39:54.191391355+00:00.db3")
 
 st.set_page_config(page_title="Debug", layout="wide")
 st.title("Debug")
-st.markdown(
-    """
+st.markdown("""
     This page allows you to execute custom SQL queries against the database.
     Use the editor below to write and test your queries.
-"""
-)
+""")
 
 if "init" not in st.session_state:
     st.session_state.query_result = None
@@ -27,7 +24,7 @@ if "init" not in st.session_state:
 st.session_state.init = True
 
 content = monaco_sql_editor(
-    value="SELECT * FROM taskstats_view LIMIT 10;",
+    value="SELECT ts, pid, tid, run_share FROM taskstats_view ORDER BY ts LIMIT 100;",
     # schema=schema,
     height="150px",
     theme="vs-dark",
@@ -37,13 +34,13 @@ content = monaco_sql_editor(
 if st.button("Run Query"):
     print(content)
     st.session_state.query_result = db.custom_query(content)
-
+    st.session_state.show_line_chart = False
 
 if st.session_state.query_result is not None:
     st.write(st.session_state.query_result)
 
     with st.container():
-        config_col1, config_col2 = st.columns(2)
+        config_col1, config_col2, _ = st.columns([0.15, 0.15, 0.7])
         with config_col1:
             x_axis = st.selectbox(
                 "X axis",
@@ -61,18 +58,21 @@ if st.session_state.query_result is not None:
 
         if st.button("Create Line Graphs", type="primary"):
             st.session_state.show_line_chart = True
-            print("show line chart", st.session_state.show_line_chart)
             st.session_state.line_chart_axis = {"x_axis": x_axis, "y_axis": y_axis}
 
-if st.session_state.show_line_chart == True:
+if st.session_state.show_line_chart and st.session_state.query_result is not None:
     if st.session_state.line_chart_axis is not None:
-        st.line_chart(st.session_state.query_result, x=st.session_state.line_chart_axis["x_axis"], 
-                      y=st.session_state.line_chart_axis["y_axis"])
+        df = st.session_state.query_result.copy()
+        x_axis = st.session_state.line_chart_axis["x_axis"]
+        y_axis = st.session_state.line_chart_axis["y_axis"]
 
+        fig = plt.figure(figsize=(12, 8))
 
-# content = st_monaco(value="# Hello world", height="600px", language="markdown")
-
-# if st.button("Show editor's content"):
-#     st.write(content)
-
-
+        label_columns = df.columns.difference([x_axis, y_axis])
+        for _, label in df.loc[:, label_columns].drop_duplicates().iterrows():
+            filtered = df
+            for col, val in zip(label_columns, label):
+                filtered = filtered.loc[filtered[col] == val]
+            plt.plot(filtered[x_axis], filtered[y_axis])
+        st.pyplot(fig, width=800)
+        plt.close()
