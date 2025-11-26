3214afa43a9dfca2fd5d56e7e12f679476aa0ff3
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-11-26 14:54:07 +0100

    Add service context to processes not in process_context

--- analysis/sql/pid_connections.sql
+++ analysis/sql/pid_connections.sql
@@ -97,13 +97,13 @@ WITH
   service_context AS (
     SELECT DISTINCT
         COALESCE(k.pod_name, d.name, tv.comm) AS service_name,
-        pc.machine_id,
-        pc.pid, 
+        COALESCE(pc.machine_id, tv.machine_id) machine_id,
+        COALESCE(pc.pid, tv.pid) pid, 
         pc.exe
     FROM process_context pc
     LEFT JOIN docker d USING (machine_id, cgroup)
     LEFT JOIN k8s k USING(machine_id, cgroup)
-    LEFT JOIN 
+    RIGHT JOIN 
         (SELECT DISTINCT machine_id, pid, tid, comm from taskstats_view where pid = tid) tv 
         ON (pc.pid = tv.pid) and (pc.pid = tv.tid) and (pc.machine_id = tv.machine_id)
   ),
