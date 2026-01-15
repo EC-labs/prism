cd4e492842bbc0ddecb6d3e565b94ce7e27bf720
landaudiogo <diogo.hewitt.landau@hotmail.com>
2026-01-15 10:18:47 +0100

    Add template variables heading in debug page

--- analysis/src/pages/debug.py
+++ analysis/src/pages/debug.py
@@ -34,6 +34,7 @@ st.session_state.init = True
 
 
 def variables_table():
+    st.markdown("### Template Variables")
     debug_variables = st.session_state.debug_variables
     st.data_editor(debug_variables, key=st.session_state.debug_data_editor_key, num_rows="dynamic")
     editor_changes = st.session_state[st.session_state.debug_data_editor_key]
